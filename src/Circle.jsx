b686cb6239cac5e6627bd2a00e03b3907ab2cacb
laurpe <laura.perttila@edu.bc.fi>
2022-03-28 17:48:52 +0300

    circle bg color changes with count

--- src/Circle.jsx
+++ src/Circle.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Circle = ({ count }) => {
+    if (count % 2 !== 0) {
+        return <div className="circle odd">{count}</div>;
+    }
     return <div className="circle">{count}</div>;
 };
 
